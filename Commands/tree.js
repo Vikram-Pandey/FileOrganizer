10599122bf4591805b1f4ec90912410800d904d6
Vikram <pandey.vikram@outlook.com>
2022-03-22 23:26:11 -0500

    CompletedTreeFunctionality

--- Commands/tree.js
+++ Commands/tree.js
@@ -1,8 +1,33 @@
 const path=require("path");
 const fs=require("fs");
 
+
 function printTree(srcPath){
-fs.readdir(srcPath,function(files){
-    
-})
+fs.readdir(srcPath,(err,files)=>{
+    if(err){
+        console.log(err);
+    }
+
+    else{
+        console.log("files in "+path.basename(srcPath)+ " Directory");
+        files.forEach(file=>{
+            let name=path.join(srcPath,file);
+            let isDirectory=fs.statSync(name).isDirectory();
+
+            if(!isDirectory){
+            
+            console.log(file.toString());
+            }
+            else{
+                console.log(file.toString());
+                printTree(name);
+            }
+        });
+    }
+});
+}
+
+
+module.exports={
+    tree:printTree
 }
--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-const { Console } = require("console");
 let helpfunc =require("./Commands/help");
 let organizefunc = require("./Commands/organize");
+let treeFunc=require("./Commands/tree");
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
 let path = inputArr[1];
 
+console.log(command);
+console.log(path);
+
 switch (command) {  //organize
     case "tree":
         console.log("Tree function called and executed succesfull on path" + path);
+        treeFunc.tree(path);
         break;
 
     case "organize":
         // console.log("Organize function called and executed successfully on path" + path);
         console.log(path);
-        organizefunc.organizefm(path);
+       // organizefunc.organizefm(path);
         break;
 
     case "help":
